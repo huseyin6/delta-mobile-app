3937311d31040a3ec1e7b13994e2ff4c8f479349
Huseyin <huseyinkarakaya006@gmail.com>
2022-07-02 18:05:29 +0300

    urlcon

--- urlconfig.js
+++ urlconfig.js
@@ -1,3 +1,4 @@
 //  http://faxriboot-env.eba-dincnkef.us-east-1.elasticbeanstalk.com/swagger-ui/#/
+// api req
 export const api =
   'http://faxriboot-env.eba-dincnkef.us-east-1.elasticbeanstalk.com/api';
