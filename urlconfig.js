8611e606a68e195307f34e3045f89104b32b7752
Huseyin <huseyinkarakaya006@gmail.com>
2022-07-14 13:39:05 +0300

    API URL has changed

--- urlconfig.js
+++ urlconfig.js
@@ -1,4 +1,4 @@
 //  http://faxriboot-env.eba-dincnkef.us-east-1.elasticbeanstalk.com/swagger-ui/#/
 // api req
 export const api =
-  'http://faxriboot-env.eba-dincnkef.us-east-1.elasticbeanstalk.com/api';
+  'https://faxriboot-env.eba-dincnkef.us-east-1.elasticbeanstalk.com/api';
